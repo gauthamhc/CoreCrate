59c82ef409d3e6ea9028059eb887ec7904e64e8f
Gautham Chengappa <gautham.hc@gmail.com>
2021-05-28 21:32:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var accordions = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+  accordions[i].onclick = function () {
+    this.classList.toggle("is-open");
+
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      // accordion is currently open, so close it
+      content.style.maxHeight = null;
+    } else {
+      // accordion is currently closed, so open it
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  };
+}
+
+const contents = document.querySelectorAll("#industry li");
+contents.forEach((content) => {
+  content.addEventListener("click", () => {
+    const createdIndustry = document.createElement("p");
+    createdIndustry.innerText = content.innerText;
+    const closeIcon = document.createElement("span");
+    closeIcon.innerHTML = "&times;";
+    createdIndustry.append(closeIcon);
+    const selectedIndustry = document.querySelector(
+      ".industries_classification"
+    );
+    selectedIndustry.append(createdIndustry);
+    createdIndustry.addEventListener("click", () => {
+      createdIndustry.remove();
+    });
+  });
+});
+
+// const deleteContents = document.querySelectorAll(
+//   ".industries_classification p"
+// );
+
+// console.log(deleteContents);
